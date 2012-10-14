a0a4b39753cc666da03ebcaca66f944507ac4eca
jason peak <jpeak5@lsu.edu>
2012-10-13 22:29:47 -0500

    clearing out old stuff

---
+++ install.literati.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s vendor/TeiInteract plugins/TeiInteract;
+ln -s vendor/TeiDisplay plugins/TeiDisplay
+ln -s vendor/Annotations plugins/Annotations;
+ln -s vendor/omeka-foundation themes/omeka-foundation;
+ln -s vendor/plugin-ItemRelations plugins/ItemRelations;
