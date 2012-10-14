10e5c9f6ec4ddad49255d046cdf7b37db27dbc8f
jason peak <jpeak5@lsu.edu>
2012-10-14 12:01:42 -0500

    deletes symlinking shell script in favor of letting our composer installer handle paths

--- install.literati.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -s ../vendor/literati/TeiInteract plugins/TeiInteract;
-ln -s ../vendor/literati/TeiDisplay plugins/TeiDisplay
-ln -s ../vendor/literati/Annotations plugins/Annotations;
-ln -s ../vendor/literati/omeka-foundation themes/omeka-foundation;
-ln -s ../vendor/literati/plugin-ItemRelations plugins/ItemRelations;
